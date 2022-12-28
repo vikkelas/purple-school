08180cb4492813033cdae32622ff133bc85b930f
vikkelas <bass89@mail.ru>
2022-12-28 14:01:08 +0300

    добавлена новая Ñновая логика - events

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+import MyEmmitter from 'events';
 import calcSum from "./sumFunc.js";
+
+const myEmitter = new MyEmmitter;
 const firstNum = +process.argv[2];
 const secondNum = +process.argv[3];
 
-calcSum(firstNum,secondNum);
+myEmitter.on('addDate', (arg1,arg2)=>{
+    myEmitter.emit('result', arg1, arg2)
+})
+
+myEmitter.on('result', (arg1, arg2)=>{
+    calcSum(arg1, arg2);
+})
+myEmitter.emit('addDate', firstNum, secondNum);
