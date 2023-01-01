52126072574490760c77c6bcddf4aeed0fbef9c3
vikkelas <bass89@mail.ru>
2023-01-01 23:05:44 +0300

    update calc-event

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
+import MyEmmitter from 'events';
 import Calc from "./Calc.js";
+
 const firstNum = Number(process.argv[2]);
 const secondNum = Number(process.argv[3]);
 const operation = process.argv[4];
-const myEmitter = new MyEmmitter;
+
 //передать три значения, где первые два аргумента числа, а третий вид операции "sum" "multiply" "division" "difference"
-import MyEmmitter from 'events';
-import calcSum from "./sumFunc.js";
 
 const myEmitter = new MyEmmitter;
-const firstNum = +process.argv[2];
-const secondNum = +process.argv[3];
-
 const calc = new Calc(firstNum,secondNum);
-calc.calcStart(operation);
 
-
-myEmitter.on('result', (arg1, arg2)=>{
-    calcSum(arg1, arg2);
+myEmitter.on('addDate', (operation)=>{
+    myEmitter.emit('result', operation)
 })
-myEmitter.emit('addDate', firstNum, secondNum);
+
+myEmitter.on('result', (operation)=>{
+    calc.calcStart(operation);
+})
+myEmitter.emit('addDate', operation);
